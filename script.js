9778039cc7404350ba1de0d0e81f069af0696d2a
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-12-01 15:09:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+      const line = document.querySelector("#line");
+      let angle = 0;
+      setInterval(() => {
+        angle += 2;
+        line.style.transform = `rotate(${angle}deg)`;
+      }, 20);
